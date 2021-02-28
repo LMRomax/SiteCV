19b0283d1b4e3ff5f8fc38061be89689b1daf0c8
LMRomax <maxencectmd.lemaitre@gmail.com>
2021-02-28 15:20:18 +0100

    intro block done

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,3 +1,12 @@
-$('.input-field').click(function(){
-    $(this).addClass('used');
+$('.input-field input').focus(function(){
+    $(this).parent().addClass('used');
+});
+
+$('.input-field input').blur(function(){
+    if($(this).val().length > 0) {
+        $(this).parent().addClass('used');
+    }
+    else {
+        $(this).parent().removeClass('used');
+    }
 });
--- menuprincipal.php
+++ menuprincipal.php
@@ -1,75 +1,97 @@
-<header class="topnav">
-        <a href="#" class="icon">
-            <i class="fa fa-bars"></i>
-        </a>
-        <div class="logo">
-            <a href="index.php">
-                <span class="sp-logo">
-                LM
-                </span>
-                axence
-            </a>
-        </div>
+<div class="intro-container">
+    <div class="intro-container--lean"></div>
+    <div class="intro-presentation">
+        <div class="intro-presentation--content">
+            <div class="intro-presentation--logo">
+                <img src="assets/images/lm.jpg" alt="Maxence Lemaître">
+            </div>
 
-        <nav class="navigation-menu">
-            <a href="#presentation" id="element-pres">
+            <div class="intro-presentation--name">
+                Maxence Lemaître
+            </div>
+            <div class="intro-presentation--devweb">
+                Développeur Web
+            </div>
+        </div>
+    </div>
+
+    <div class="arrow"></div>
+</div>
+
+<div class="menu--nav">
+    <a href="#" class="icon">
+        <i class="fa fa-bars"></i>
+    </a>
+
+    <nav class="navigation-menu">
+        <a href="#presentation" id="element-pres">
+            <span>Présentation</span>
+        </a>
+
+        <a href="#competences" id="element-comp">
+            <span>Compétences</span>
+        </a>
+
+        <a href="#projets" id="element-projets">
+            <span>Projets</span>
+        </a>
+    </nav>
+
+    <div class="logo">
+        <a href="index.php">
+            <span class="sp-logo">
+                LM
+            </span>
+        </a>
+    </div>
+
+    <nav class="navigation-menu">
+        <a href="#experiences" id="element-exp">
+            <span>Experiences</span>
+        </a>
+
+        <a href="#references" id="element-ref">
+            <span>Références</span>
+        </a>
+
+        <a href="#contact" id="element-contact">
+            <span>Contact</span>
+        </a>
+    </nav>
+
+</div>
+
+<div class="responsive-mobile-nav responsive-mobile-drawer responsive-mobile-drawer--west">
+    <div class="responsive-mobile-container">
+        <a href="#" class="responsive-mobile-close">
+            <i class="fa fa-times"></i>
+        </a>
+
+        <nav class="responsive-mobile-menu">
+            <a href="#presentation">
                 <span>Présentation</span>
             </a>
 
-            <a href="#competences" id="element-comp">
+            <a href="#competences">
                 <span>Compétences</span>
             </a>
 
-            <a href="#projets" id="element-projets">
+            <a href="#projets">
                 <span>Projets</span>
             </a>
 
-            <a href="#experiences" id="element-exp">
+            <a href="#experiences">
                 <span>Experiences</span>
             </a>
 
-            <a href="#references" id="element-ref">
+            <a href="#references">
                 <span>Références</span>
             </a>
 
-            <a href="#contact" id="element-contact">
+            <a href="#contact">
                 <span>Contact</span>
             </a>
         </nav>
 
-    </header>
-
-    <div class="responsive-mobile-nav responsive-mobile-drawer responsive-mobile-drawer--west">
-        <div class="responsive-mobile-container">
-            <a href="#" class="responsive-mobile-close">
-                <i class="fa fa-times"></i>
-            </a>
-
-          <nav class="responsive-mobile-menu">
-              <a href="#presentation">
-                  <span>Présentation</span>
-              </a>
-
-              <a href="#competences">
-                  <span>Compétences</span>
-              </a>
-
-              <a href="#projets">
-                  <span>Projets</span>
-              </a>
-
-              <a href="#experiences">
-                  <span>Experiences</span>
-              </a>
-
-              <a href="#references">
-                  <span>Références</span>
-              </a>
-
-              <a href="#contact">
-                  <span>Contact</span>
-              </a>
-          </nav>
-
-        </div>
     </div>
+</div>
