419cdfea2dc2dcef74298c6c511d6e03bf47c65c
LMRomax <maxencectmd.lemaitre@gmail.com>
2021-03-01 12:24:14 +0100

    menu remove slick error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,35 @@
-$(document).ready(function() {
-    setTimeout(function(){ 
+$(document).ready(function () {
+    setTimeout(function () {
         $('#loading').css('display', 'none');
         $('#is-load').css('display', 'block');
-        if($(window).width() <= 768) {
+        if ($(window).width() <= 768) {
             $('.menu--nav').css('display', 'none');
             $('.responsive-mobile-nav').css('display', 'block');
+            if ($('.navigation-menu--responsive').hasClass('slick-initialized') == false) {
+                $('.navigation-menu--responsive').slick({
+                    infinite: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    variableWidth: false,
+                    centerMode: true,
+                });
+            }
+        }
+        else if ($(window).width() > 768) {
+            $('.menu--nav').css('display', 'block');
+            $('.responsive-mobile-nav').css('display', 'none');
+            if ($('.navigation-menu--responsive').hasClass('slick-initialized') == true) {
+                $('.navigation-menu--responsive').slick('unslick');
+            }
+        }
+    }, 1000);
+});
+
+$(window).resize(function () {
+    if ($(window).width() <= 768) {
+        $('.menu--nav').css('display', 'none');
+        $('.responsive-mobile-nav').css('display', 'block');
+        if ($('.navigation-menu--responsive').hasClass('slick-initialized') == false) {
             $('.navigation-menu--responsive').slick({
                 infinite: true,
                 slidesToShow: 3,
@@ -13,28 +38,12 @@ $(document).ready(function() {
                 centerMode: true,
             });
         }
-        else if($(window).width() > 768) {
-            $('.menu--nav').css('display', 'block');
-            $('.responsive-mobile-nav').css('display', 'none');
-        }
-    }, 1000);
-});
-
-$(window).resize(function() {
-    console.log($(window).width());
-    if($(window).width() <= 768) {
-        $('.menu--nav').css('display', 'none');
-        $('.responsive-mobile-nav').css('display', 'block');
-        $('.navigation-menu--responsive').slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            variableWidth: false,
-            centerMode: true,
-        });
     }
-    else if($(window).width() > 768) {
+    else if ($(window).width() > 768) {
         $('.menu--nav').css('display', 'block');
         $('.responsive-mobile-nav').css('display', 'none');
+        if ($('.navigation-menu--responsive').hasClass('slick-initialized') == true) {
+            $('.navigation-menu--responsive').slick('unslick');
+        }
     }
 });
