b305e9b0b2f49d0539ef432dd6b4072a82e115b5
LMRomax <maxencectmd.lemaitre@gmail.com>
2021-03-01 20:41:27 +0100

    presentation to begin

--- views/presentation.php
+++ views/presentation.php
@@ -1,7 +1,30 @@
-<section id="presentation" class="presentation">
-    <div class="circle">
+<section id="presentation" class="container section-wrapper">
+    <div class="data-card">
         <div class="title">
             Présentation
+        </div>
+        <div class="description-content">
+            Passionné par l'informatique depuis mon adolescence, j'ai décidé d'effectuer mes études dans ce domaine. 
+            La découverte de la programmation web fut pour moi une révélation, et j'ai alors décidé de poursuivre mon 
+            parcours dans cette voie. Véritable passionné, désireux de produire le meilleur travail qui soit, je suis 
+            par la suite devenu autodidacte et j'ai également fondé mon auto-entreprise.
+        </div>
+        <div class="contact-details-content">
+            <p>
+                <strong>Adresse :</strong>
+                <span>141 avenue du Roy de Blicquy 59154 Crespin</span>
+            </p>
+            <p>
+                <strong>E-mail :</strong>
+                <span>maxencectmd.lemaitre@gmail.com</span>
+            </p>
+            <p>
+                <strong>Téléphone :</strong>
+                <span>06 26 23 71 81</span>
+            </p>
+        </div>
+        <div class="social-network-content">
+
         </div>
     </div>
 </section>
