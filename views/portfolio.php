44e854699ef057bbdca8e71d89280f0be5bb5d54
LMRomax <maxencectmd.lemaitre@gmail.com>
2021-03-03 18:59:45 +0100

    animation portfolio

--- views/portfolio.php
+++ views/portfolio.php
@@ -6,15 +6,70 @@
     <div class="portfolio-list">
         <div class="row portfolio-row">
             <div class="col-md-4 portfolio-card">
-                <div class="img-projet spinforwin-projet"></div>
+                <div class="img-projet spinforwin-projet">
+                    <div class="img-projet-mask">
+                        <div class="img-projet-mask--content">
+                            <div class="img-projet-mask--title">
+                                SpinForWin
+                            </div>
+                            <div class="plus-icon">
+                                <img src="assets/images/lupe.png" alt="Plus icon">
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col-md-3 portfolio-card">
-                <div class="img-projet yoo-projet"></div>
-                <div class="img-projet dubaijetski-projet"></div>
+                <div class="img-projet yoo-projet">
+                    <div class="img-projet-mask">
+                        <div class="img-projet-mask--content">
+                            <div class="img-projet-mask--title">
+                                Yoo (Your Own Organization)
+                            </div>
+                            <div class="plus-icon">
+                                <img src="assets/images/lupe.png" alt="Plus icon">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="img-projet dubaijetski-projet">
+                    <div class="img-projet-mask">
+                        <div class="img-projet-mask--content">
+                            <div class="img-projet-mask--title">
+                                Dubaï Jetski
+                            </div>
+                            <div class="plus-icon">
+                                <img src="assets/images/lupe.png" alt="Plus icon">
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="col-md-5 portfolio-card">
-                <div class="img-projet bell-projet"></div>
-                <div class="img-projet bt-projet"></div>
+                <div class="img-projet bell-projet">
+                    <div class="img-projet-mask">
+                        <div class="img-projet-mask--content">
+                            <div class="img-projet-mask--title">
+                                Bell
+                            </div>
+                            <div class="plus-icon">
+                                <img src="assets/images/lupe.png" alt="Plus icon">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="img-projet bt-projet">
+                    <div class="img-projet-mask">
+                        <div class="img-projet-mask--content">
+                            <div class="img-projet-mask--title">
+                                British Telecom
+                            </div>
+                            <div class="plus-icon">
+                                <img src="assets/images/lupe.png" alt="Plus icon">
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
