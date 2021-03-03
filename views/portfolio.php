36fe33fbc6a86fd39ee6d9fab09d611a24e6a394
LMRomax <maxencectmd.lemaitre@gmail.com>
2021-03-03 15:36:46 +0100

    portfolio img

--- views/portfolio.php
+++ views/portfolio.php
@@ -6,25 +6,15 @@
     <div class="portfolio-list">
         <div class="row portfolio-row">
             <div class="col-md-4 portfolio-card">
-                <div class="img-projet">
-                    <img src="assets/images/portfolio/hnqe3blabfb9mhhiapcw.jpg" alt="SpinForWin">
-                </div>
+                <div class="img-projet spinforwin-projet"></div>
             </div>
             <div class="col-md-3 portfolio-card">
-                <div class="img-projet">
-                    <img src="assets/images/portfolio/ctwvwzcfll0xlldj7ovo.jpg" alt="Yoo">
-                </div>
-                <div class="img-projet">
-                    <img src="assets/images/portfolio/cd7r0scso6bxiggsrwhw.jpg" alt="Dubaï Jetski">
-                </div>
+                <div class="img-projet yoo-projet"></div>
+                <div class="img-projet dubaijetski-projet"></div>
             </div>
             <div class="col-md-5 portfolio-card">
-                <div class="img-projet">
-                    <img src="assets/images/portfolio/2021-01-21-19-05-echange.bell.ca.png" alt="Bell">
-                </div>
-                <div class="img-projet">
-                    <img src="assets/images/portfolio/2021-01-21-19-03-my.bt.com.png" alt="Dubaï Jetski">
-                </div>
+                <div class="img-projet bell-projet"></div>
+                <div class="img-projet bt-projet"></div>
             </div>
         </div>
     </div>
